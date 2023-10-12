fea8b8f8d93b901d44630a5a6d9a4e9822167198
Dambé <ansthaanselme@gmail.com>
2023-10-12 17:08:17 +0400

    Add router capabilities

---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function ProductDetail() {
+    return (     
+            
+            <Typography variant="h4">Product Detail</Typography>
+
+    )
+}
